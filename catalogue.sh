41a3002eee081303f0dcef48e6cf7c72c134c369
sarathdevOPS1 <saratdev9697@gmail.com>
2025-03-07 12:34:38 +0530

    catalogue script

--- catalogue.sh
+++ catalogue.sh
@@ -1,5 +1,3 @@
-cp mongodb.repo /etc/yum.repos.d/mongo.repo
-cp catalogue.service /etc/systemd/system/catalogue.service
 
 dnf module disable nodejs -y
 dnf module enable nodejs:20 -y
@@ -16,7 +14,12 @@ unzip /tmp/catalogue.zip
 cd /app
 npm install
 
+cp catalogue.service /etc/systemd/system/catalogue.service
+
+cp mongodb.repo /etc/yum.repos.d/mongo.repo
+
 dnf install mongodb-mongosh -y
+
 mongosh --host mongodb.sarathdev96.online </app/db/master-data.js
 
 systemctl daemon-reload
