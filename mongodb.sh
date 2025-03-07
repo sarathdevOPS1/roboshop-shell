84693d3c4a6aa4b45f063114360b1c3ca5aeda62
sarathdevOPS1 <saratdev9697@gmail.com>
2025-03-07 19:53:40 +0530

    catalogue script

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,7 @@
 cp mongodb.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org -y
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' +++++++++++++++++++++++++++++++
 
 systemctl enable mongod
 systemctl restart mongod
