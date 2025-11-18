6df6f74bf70ae95ccc1df6e9deb2978975ab8b3b
sarathdevOPS1 <saratdev9697@gmail.com>
2025-11-18 12:54:07 +0530

    adding print statement

--- dispatch.sh
+++ dispatch.sh
@@ -1,19 +1,28 @@
+echo -e "\e[35m copy dispatch service file \e[0m"
+cp dispatch.service /etc/systemd/system/dispatch.service
+
+echo -e "\e[35m install Golang \e[0m"
 dnf install golang -y
 
+echo -e "\e[35m add application user \e[0m"
 useradd roboshop
 
+echo -e "\e[35m create application directory \e[0m"
 mkdir /app
 
+echo -e "\e[35m downlaod apllication content \e[0m"
 curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch-v3.zip
 cd /app
+
+echo -e "\e[35m extract application content \e[0m"
 unzip /tmp/dispatch.zip
 
-cd /app
+echo -e "\e[35m download apllication dependencies \e[0m"
 go mod init dispatch
 go get
 go build
 
+echo -e "\e[35m start application service  \e[0m"
 systemctl daemon-reload
-
 systemctl enable dispatch
 systemctl start dispatch
