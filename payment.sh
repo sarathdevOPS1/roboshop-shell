677e847c60dc5dba55a048951378d522533e92a4
sarathdevOPS1 <saratdev9697@gmail.com>
2025-11-18 22:59:11 +0530

    adding payment.sh

--- payment.sh
+++ payment.sh
@@ -1,16 +1,27 @@
+echo -e "$color copy payment service file $no_color"
 cp payment.service /etc/systemd/system/payment.service
 
+echo -e "$color copy dispatch service file $no_color"
 dnf install python3 gcc python3-devel -y
+
+echo -e "$color adding application user $no_color"
 useradd roboshop
 
+echo -e "$color create application directory $no_color"
+rm -rf /app
 mkdir /app
+
+echo -e "$color download application content $no_color"
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip
 cd /app
+
+echo -e "$color  $no_color"extract application content
 unzip /tmp/payment.zip
 
-cd /app
+echo -e "$color download application dependencies $no_color"
 pip3 install -r requirements.txt
 
+echo -e "$color start application service $no_color"
 systemctl daemon-reload
 systemctl enable payment
 systemctl restart payment
