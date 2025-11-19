04e460163605d4bce4c7bf76544a03444b5b0546
sarathdevOPS1 <saratdev9697@gmail.com>
2025-11-19 14:09:30 +0530

    adding status check

--- common.sh
+++ common.sh
@@ -1,24 +1,26 @@
 color="\e[35m"
 no_color="\e[0m"
+log_file=/tmp/roboshop.log
+rm -f $log_file
 
 
 app_prerequisites() {
   echo -e "$color add application user $no_color"
-  useradd roboshop
+  useradd roboshop &>>$log_file
   echo $?
 
   echo -e "$color create application directory $no_color"
-  rm -rf /app
-  mkdir /app
+  rm -rf /app &>>$log_file
+  mkdir /app &>>$log_file
   echo $?
 
   echo -e "$color downlaod apllication content $no_color"
-  curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip
+  curl -L -o /tmp/$app_name.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$log_file
   echo $?
 
   cd /app
 
   echo -e "$color extract application content $no_color"
-  unzip /tmp/$app_name.zip
+  unzip /tmp/$app_name.zip &>>$log_file
   echo $?
 }
--- dispatch.sh
+++ dispatch.sh
@@ -2,23 +2,23 @@ source common.sh
 app_name=dispatch
 
 echo -e "$color copy dispatch service file $no_color"
-cp dispatch.service /etc/systemd/system/dispatch.service
+cp dispatch.service /etc/systemd/system/dispatch.service &>>$log_file
 echo $?
 
 echo -e "$color install Golang $no_color"
-dnf install golang -y
+dnf install golang -y &>>$log_file
 echo $?
 
 app_prerequisites
 
 echo -e "$color download apllication dependencies $no_color"
 go mod init dispatch
-go get
-go build
+go get &>>$log_file
+go build &>>$log_file
 echo $?
 
 echo -e "$color start application service  $no_color"
-systemctl daemon-reload
-systemctl enable dispatch
-systemctl start dispatch
+systemctl daemon-reload &>>$log_file
+systemctl enable dispatch &>>$log_file
+systemctl start dispatch &>>$log_file
 echo $?
