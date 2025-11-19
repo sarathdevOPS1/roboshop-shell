c8c4da498cd9aa8c662e4f78d21c95611b889fa5
sarathdevOPS1 <saratdev9697@gmail.com>
2025-11-19 13:52:21 +0530

    adding status check

--- dispatch.sh
+++ dispatch.sh
@@ -3,9 +3,11 @@ app_name=dispatch
 
 echo -e "$color copy dispatch service file $no_color"
 cp dispatch.service /etc/systemd/system/dispatch.service
+echo $?
 
 echo -e "$color install Golang $no_color"
 dnf install golang -y
+echo $?
 
 app_prerequisites
 
@@ -13,8 +15,10 @@ echo -e "$color download apllication dependencies $no_color"
 go mod init dispatch
 go get
 go build
+echo $?
 
 echo -e "$color start application service  $no_color"
 systemctl daemon-reload
 systemctl enable dispatch
 systemctl start dispatch
+echo $?
